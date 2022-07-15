380cf39fa7a6218ab00c9f5b9c1dd29788b71d65
Gigsman2022 <adesh.gigsman@gmail.com>
2022-07-15 17:12:05 +0530

    Updated the Buildpack

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //   BASE_URL: "https://gigzman-backend.herokuapp.com",
-  BASE_URL: "http://localhost:5001",
+  BASE_URL: "https://gigzman-backend.herokuapp.com",
+  // BASE_URL: "http://localhost:5001",
 };
